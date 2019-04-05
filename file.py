7e00bcc03dd8935c60d68cae7d418aa6430a7cd4
Valeriy Matskevich <39431974+sadboyyy@users.noreply.github.com>
2019-04-05 17:20:49 +0500

    it can show you map after getting coordinates and scale from cmd

--- file.py
+++ file.py
@@ -1,16 +1,12 @@
+from PIL import Image
 import requests
-from urllib.parse import urlsplit
+from io import BytesIO
 
 # 37.677751 55.75771
 
 def requests_image(file_url):
     i = requests.get(file_url)
-    if i.status_code == requests.codes.ok:
-        with open(str(file_url[:25] + '.png'), 'wb') as file:
-            file.write(i.content)
-        return "Запрос успешно выполнен"
-    else:
-        return "Не удалось выполнить запрос :("
+    return i.content
 
 
 print('Введите координаты местности, два действительных числа через пробел:')
@@ -18,7 +14,10 @@ l, d = map(float, input().split())
 print('Введите масштаб, вещественное число от 100 до 90000:')
 m = float(input())
 
-static_request = "https://static-maps.yandex.ru/1.x/?ll={},{}&spn={},{}&l=map".format(
+url = "https://static-maps.yandex.ru/1.x/?ll={},{}&spn={},{}&l=map".format(
     d, l, 90 / m, 90 / m)
-print(static_request)
-print(requests_image(static_request))
+print(url)
+byte = requests_image(url)
+response = requests.get(url)
+img = Image.open(BytesIO(response.content))
+img.save("map.png")
