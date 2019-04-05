40ff2e645b51a127b56e0ba69b7eefaf213b9011
Valeriy Matskevich <39431974+sadboyyy@users.noreply.github.com>
2019-04-05 17:23:04 +0500

    Add files via upload

--- file.py
+++ file.py
@@ -2,22 +2,13 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-# 37.677751 55.75771
-
-def requests_image(file_url):
-    i = requests.get(file_url)
-    return i.content
-
-
 print('Введите координаты местности, два действительных числа через пробел:')
 l, d = map(float, input().split())
 print('Введите масштаб, вещественное число от 100 до 90000:')
 m = float(input())
 
 url = "https://static-maps.yandex.ru/1.x/?ll={},{}&spn={},{}&l=map".format(
-    d, l, 90 / m, 90 / m)
-print(url)
-byte = requests_image(url)
+    l, d, 90 / m, 90 / m)
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
 img.save("map.png")
